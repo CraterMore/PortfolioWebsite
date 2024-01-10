b6a70d906cbd612676364f7594f128a80a17de84
CraterMore <ctm6704@yahoo.com>
2024-01-10 15:37:26 -0500

    New project established with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      'text': '#00030f',
+      'background': '#f5f7ff',
+      'primary': '#b1eb9e',
+      'secondary': '#7edafb',
+      'accent': '#235b53',
+     },
+     fontSize: {
+      sm: '0.750rem',
+      base: '1rem',
+      xl: '1.333rem',
+      '2xl': '1.777rem',
+      '3xl': '2.369rem',
+      '4xl': '3.158rem',
+      '5xl': '4.210rem',
+    },
+    fontFamily: {
+      heading: 'Roboto',
+      body: 'Inter',
+    },
+    fontWeight: {
+      normal: '400',
+      bold: '700',
+    }, 
+    extend: {},
+  },
+  plugins: [],
+}
+
